fd1bc925c8a7345ebdbc1ec6dde2c2e8f0c31ab5
Ilija Tovilo <ilija.tovilo@me.com>
2021-09-16 09:18:45 +0200

    Add sample project

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createForm } from 'svelte-forms-lib';
+
+    const { form, handleSubmit, handleChange } = createForm({
+        initialValues: { a: 1 },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    });
+</script>
+
+<form on:submit={handleSubmit}>
+    <select name="a" value={$form.a} on:change={handleChange}>
+        <option />
+        <option value={0}>Foo</option>
+        <option value={1}>Bar</option>
+        <option value={2}>Baz</option>
+    </select>
+    <button type="submit">submit</button>
+</form>
